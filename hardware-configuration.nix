5ab63b6a1d6fb251fd752e5dffc17f871e6935ff
Simon Hauser <Simon-Hauser@outlook.de>
2022-01-02 15:35:27 +0100

    regenerate hardware-configuration

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,37 +5,32 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [
-    config.boot.kernelPackages.rtl88x2bu
-  ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/d4bf0ec3-4905-498d-bbaa-16a9453ebf84";
+    { device = "/dev/disk/by-uuid/d4bf0ec3-4905-498d-bbaa-16a9453ebf84";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/3A09-1DA5";
+    { device = "/dev/disk/by-uuid/3A09-1DA5";
       fsType = "vfat";
     };
 
   fileSystems."/mnt/internal" =
-    {
-      device = "/dev/disk/by-uuid/6de62098-e8bd-4e0d-8d4a-253c775dd616";
+    { device = "/dev/disk/by-uuid/6de62098-e8bd-4e0d-8d4a-253c775dd616";
       fsType = "ext4";
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/003454e5-b1c4-43ac-aa8e-a07838ad9382"; }];
+    [ { device = "/dev/disk/by-uuid/003454e5-b1c4-43ac-aa8e-a07838ad9382"; }
+    ];
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
