23bb6cf2dae9cd9bf413f82e34adb2e5ce204f2a
Simon Hauser <Simon-Hauser@outlook.de>
2022-05-15 07:44:35 +0200

    add gnome config

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@ in
       ./1systems/pipewire.nix
       ./1systems/bluetooth.nix
       ./2configs/dwm.nix
+      ./2configs/gnome.nix
       ./2configs/virt.nix
       ./3modules/news.nix
       ./3modules/mail.nix
